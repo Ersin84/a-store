aebb53d3e6b265cde723a540e1f00f94626d7b8f
wromo <info.wromo@gmail.com>
2022-03-26 15:18:52 +0100

    add

--- next.config.js
+++ next.config.js
@@ -40,8 +40,8 @@ module.exports = withPWA({
   },
 });
 
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-   enabled: process.env.ANALYZE === 'true',
- });
+//const withBundleAnalyzer = require('@next/bundle-analyzer')({
+//   enabled: process.env.ANALYZE === 'true',
+// });
 
- module.exports = withBundleAnalyzer({});
+// module.exports = withBundleAnalyzer({});
