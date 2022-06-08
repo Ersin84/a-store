984504ed825df842348e4c61e27fe1d97d8acc97
wromo <info.wromo@gmail.com>
2022-06-08 12:33:54 +0200

    add080622-3

--- src/pages/404.js
+++ src/pages/404.js
@@ -31,5 +31,8 @@ const NotFound = () => {
     </>
   );
 };
+import ReactGA from 'react-ga';
+const TRACKING_ID = "G-1FPWCHHJVM"; // OUR_TRACKING_ID
+ReactGA.initialize(TRACKING_ID);
 
 export default NotFound;
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -36,5 +36,7 @@ class MyDocument extends Document {
     );
   }
 }
-
+import ReactGA from 'react-ga';
+const TRACKING_ID = "G-1FPWCHHJVM"; // OUR_TRACKING_ID
+ReactGA.initialize(TRACKING_ID);
 export default MyDocument;
