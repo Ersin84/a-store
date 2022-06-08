4d66d3faaf3a1903fd300749ce5bcaa3152f218a
wromo <info.wromo@gmail.com>
2022-06-08 12:04:18 +0200

    add080622-2

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -26,4 +26,7 @@ function MyApp({ Component, pageProps }) {
   );
 }
 
+import ReactGA from 'react-ga';
+const TRACKING_ID = "G-1FPWCHHJVM"; // OUR_TRACKING_ID
+ReactGA.initialize(TRACKING_ID);
 export default MyApp;
