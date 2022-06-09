5cb7216ea28c09b14a907b36a2098e5cb90ba555
wromo <info.wromo@gmail.com>
2022-06-09 06:45:52 +0200

    add090622-18

--- src/component/login/Common.js
+++ src/component/login/Common.js
@@ -8,14 +8,6 @@ import ResetPassword from '@component/login/ResetPassword';
 import useLoginSubmit from '@hooks/useLoginSubmit';
 import { useGoogleOneTapLogin } from 'react-google-one-tap-login';
 
-useGoogleOneTapLogin({
-    onError: error => console.log(error),
-    onSuccess: response => console.log(response),
-    googleAccountConfigs: {
-      client_id: 162887562432-b05sed3sm3ajh9h055hp1n5fi667g4i1.apps.googleusercontent.com
-    },
-  });
-
 
 const Common = ({ setModalOpen }) => {
   const [showRegister, setShowRegister] = useState(false);
