419edf29f4debad107ffc30067e2d7216e31d502
wromo <info.wromo@gmail.com>
2022-06-19 22:03:51 +0200

    add-1906-05

--- src/component/header/PageHeader.js
+++ src/component/header/PageHeader.js
@@ -1,11 +1,7 @@
 import React from "react";
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-J5C7KBJ229"></script>
-
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-
 gtag('config', 'G-J5C7KBJ229');
+
 {description && <meta name="description" content={description} />}
 
 const PageHeader = ({ title }) => {
