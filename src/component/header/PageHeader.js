dca99170913e4dce690d4abca5689578a7cb58b5
wromo <info.wromo@gmail.com>
2022-06-19 22:05:55 +0200

    add-1906-06

--- src/component/header/PageHeader.js
+++ src/component/header/PageHeader.js
@@ -1,6 +1,5 @@
 import React from "react";
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-J5C7KBJ229"></script>
-gtag('config', 'G-J5C7KBJ229');
 
 {description && <meta name="description" content={description} />}
 
